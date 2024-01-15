1057e54962730b0854ea344654c5676b929dfb88
MohammedKreishan <mk.kreishan@gmail.com>
2024-01-16 02:25:28 +0300

    Backend + FrontEnd >> 9

--- frontend/src/components/Dashboard/Dashboard.js
+++ frontend/src/components/Dashboard/Dashboard.js
@@ -12,7 +12,6 @@ export default function () {
   const test = localStorage.getItem("token");
   const [SelectCourse, setSelectCourse] = useState("");
 
-
   const redirect = useNavigate();
   useEffect(() => {
     axios
@@ -32,84 +31,102 @@ export default function () {
 
   return (
     <div>
-      <h1>Dashboard</h1>
+      <h1
+        style={{
+          textAlign: "center",
+          fontSize: "2.5rem",
+          marginTop: "20px",
+          marginBottom: "20px",
+          color: "#007BFF",
+        }}
+      >
+        Dashboard
+      </h1>{" "}
       <Container style={{ margin: "5px" }} fluid>
         <Row xs={1} md={3} className="g-4">
-          {Courses.map((courses, indx) => (
-            <Col key={indx}>
-              <Card bg="light" className="h-100 d-flex flex-column">
-                <Card.Body className="d-flex flex-column">
-                  <Card.Title>{courses.courseId.courseTitle}</Card.Title>
-                  <Card.Text>{courses.courseId.Discription}</Card.Text>
-                  <Card.Text style={{ textAlign: "left", fontSize: "20px" }}>
-                    {" "}
-                    By{"  "}
-                    {courses.courseId.courseInstructor.firstName}{" "}
-                    {courses.courseId.courseInstructor.lastName}
-                  </Card.Text>
-                  <div className="mt-auto">
-                    <Button
-                      style={{ margin: "5px" }}
-                      className="mb-2"
-                      variant="primary"
-                      onClick={() => {
-                        axios
-                          .get(
-                            `http://localhost:5000/courses/search_2/${courses.courseId._id}`,
-                            {},
-                            {},
-                            {
-                              headers: {
-                                Authorization: `Bearer ${test}`,
-                              },
-                            }
-                          )
-                          .then((res) => {
-                            console.log(
-                              "res",
-                              res.data.course
-                            );
-                            setSelectCourse(res.data.course);
-                            redirect(`/users/onecourse/${courses.courseId._id}`);
-                          })
-                          .catch((err) => {
-                            console.log(err);
-                          });
-                      }}
-                    >
-                      Viwe Course
-                    </Button>
-                    <Button
-                      style={{ margin: "5px" }}
-                      className="mb-2"
-                      variant="primary"
-                      onClick={() => {
-                        console.log(test);
-                        axios
-                          .delete(
-                            `http://localhost:5000/courses/${courses._id}`,
-                            {},
-                            {
-                              headers: {
-                                Authorization: `Bearer ${test}`,
-                              },
-                            }
-                          )
-                          .then((res) => {
-                            console.log("res", res);
-                          })
-                          .catch((err) => {
-                            console.log(err);
-                          });
-                      }}
-                    >
-                      Delet Course
-                    </Button>
-                  </div>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
+          {Courses.length === 0 ? (
+            <p
+              className="mx-auto"
+              style={{ fontSize: "1.5rem", color: "#007BFF" }}
+            >
+              No courses found. Enroll in a course to get started!
+            </p>
+          ) : (
+            Courses.map((courses, indx) => (
+              <Col key={indx}>
+                <Card bg="light" className="h-100 d-flex flex-column">
+                  <Card.Body className="d-flex flex-column">
+                    <Card.Title>{courses.courseId.courseTitle}</Card.Title>
+                    <Card.Text>{courses.courseId.Discription}</Card.Text>
+                    <Card.Text style={{ textAlign: "left", fontSize: "20px" }}>
+                      {" "}
+                      By{"  "}
+                      {courses.courseId.courseInstructor.firstName}{" "}
+                      {courses.courseId.courseInstructor.lastName}
+                    </Card.Text>
+                    <div className="mt-auto">
+                      <Button
+                        style={{ margin: "5px" }}
+                        className="mb-2"
+                        variant="primary"
+                        onClick={() => {
+                          axios
+                            .get(
+                              `http://localhost:5000/courses/search_2/${courses.courseId._id}`,
+                              {},
+                              {},
+                              {
+                                headers: {
+                                  Authorization: `Bearer ${test}`,
+                                },
+                              }
+                            )
+                            .then((res) => {
+                              console.log("res", res.data.course);
+                              setSelectCourse(res.data.course);
+                              redirect(
+                                `/users/onecourse/${courses.courseId._id}`
+                              );
+                            })
+                            .catch((err) => {
+                              console.log(err);
+                            });
+                        }}
+                      >
+                        View Course
+                      </Button>
+                      <Button
+                        style={{ margin: "5px" }}
+                        className="mb-2"
+                        variant="primary"
+                        onClick={() => {
+                          console.log(test);
+                          axios
+                            .delete(
+                              `http://localhost:5000/courses/${courses._id}`,
+                              {},
+                              {
+                                headers: {
+                                  Authorization: `Bearer ${test}`,
+                                },
+                              }
+                            )
+                            .then((res) => {
+                              console.log("res", res);
+                            })
+                            .catch((err) => {
+                              console.log(err);
+                            });
+                        }}
+                      >
+                        Delete Course
+                      </Button>
+                    </div>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))
+          )}
         </Row>
       </Container>
     </div>
