0d0f9b95d212aec985084b552bd5ddf748e3d14a
MohammedKreishan <mk.kreishan@gmail.com>
2024-01-15 23:56:16 +0300

    Backend + FrontEnd >> 8

--- frontend/src/components/Admin/Admin.js
+++ frontend/src/components/Admin/Admin.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect,useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 
 import axios from "axios";
 import Button from "react-bootstrap/Button";
@@ -7,26 +7,31 @@ import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import { useNavigate } from "react-router-dom";
+import Accordion from "react-bootstrap/Accordion";
 
 import { Container } from "react-bootstrap";
 import InputGroup from "react-bootstrap/InputGroup";
 import { userContext } from "../../App";
 export default function () {
-  const [userLastName, setUserFirstName] = useState("");
-  const [userFirstName, setUserLastName] = useState("");
-  const [userEmail, setUserEmail] = useState("");
-  const [userPassword, setUserPassword] = useState("");
-  const [userCountry, setUserCountry] = useState("");
-  const [userAge, setUserAge] = useState(0);
-  const [result, setUserResult] = useState("");
-  const { setToggle, role ,setRole} = useContext(userContext);
+  const [courseRate, setCourseRate] = useState("");
+  const [courseTitle, setCourseTitle] = useState("");
+  const [courseDiscription, setCourseDiscription] = useState("");
+  const [courseCategory, setCourseCategory] = useState("");
+  const [coursePicture, setCoursePicture] = useState("");
+  const { setToggle, role, setRole } = useContext(userContext);
 
   const [Courses, setCourses] = useState([]);
   console.log(role);
 
+
+  
   useEffect(() => {
     axios
-      .get(`http://localhost:5000/courses/search_1/${localStorage.getItem("userId")}`)
+      .get(
+        `http://localhost:5000/courses/search_1/${localStorage.getItem(
+          "userId"
+        )}`
+      )
       .then((res) => {
         setCourses(res.data.courses);
         console.log(res.data.courses);
@@ -72,10 +77,7 @@ export default function () {
                             }
                           )
                           .then((res) => {
-                            console.log(
-                              "res",
-                              res.data.course
-                            );
+                            console.log("res", res.data.course);
                             // setSelectCourse(res.data.course);
                             // redirect(`/users/onecourse/${courses.courseId._id}`);
                           })
@@ -122,7 +124,7 @@ export default function () {
       <h1>Add Course</h1>
       <hr style={{ margin: "10px 0" }} />
 
-      <Form>
+      {/* <Form>
       <Row className="mb-3">
         <Form.Group as={Col} controlId="formGridEmail">
           <Form.Label>Course Name</Form.Label>
@@ -131,7 +133,7 @@ export default function () {
 
         <Form.Group as={Col} controlId="formGridPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="text" placeholder="Enter Dicription of Course" />
         </Form.Group>
       </Row>
 
@@ -172,9 +174,109 @@ export default function () {
       <Button variant="primary" type="submit">
         Submit
       </Button>
-    </Form>
+    </Form> */}
 
-    
+      <Container
+        className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "100vh" }}
+      >
+        {" "}
+        <Form>
+          <Row className="mb-4">
+            <Form.Group as={Row} controlId="formGridCourseTitle">
+              <Form.Label>Course Title</Form.Label>
+              <Form.Control
+                onChange={(e) => {
+                  setCourseTitle(e.target.value);
+                }}
+                type="text"
+                placeholder="Enter Course Title"
+              />
+            </Form.Group>
+
+            <Form.Group as={Row} controlId="formGridCourseDescription">
+              <Form.Label>Course Description</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter Course Description"
+              />
+            </Form.Group>
+          </Row>
+
+          <Row className="mb-3">
+            <Form.Group as={Col} controlId="formGridCourseRate">
+              <Form.Label>Course Rate</Form.Label>
+              <Form.Control type="text" placeholder="Enter Course Rate" />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridCourseInstructor">
+              <Form.Label>Course Instructor</Form.Label>
+              <Form.Control type="text" placeholder="Enter Course Instructor" />
+            </Form.Group>
+          </Row>
+
+          <Row className="mb-3">
+            <Form.Group as={Col} controlId="formGridCourseCategory">
+              <Form.Label>Course Category</Form.Label>
+              <Form.Control type="text" placeholder="Enter Course Category" />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridCoursePicture">
+              <Form.Label>Course Picture</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter Course Picture URL"
+              />
+            </Form.Group>
+          </Row>
+
+          <Row className="mb-3">
+            <Form.Group as={Col} controlId="formGridState">
+              <Form.Label>Categories</Form.Label>
+              <Form.Select defaultValue="Choose...">
+                <option>Choose...</option>
+                <option>...</option>
+              </Form.Select>
+            </Form.Group>
+          </Row>
+
+          <Form.Group className="mb-3" id="formGridCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+
+          <Button
+            onClick={() => {
+              axios
+                .post(
+                  `http://localhost:5000/courses`,
+                  {
+                    courseTitle: courseTitle,
+                    courseDiscription: courseDiscription,
+                    courseRate: courseRate,
+                    coursePicture: coursePicture,
+                    courseCategory: courseCategory,
+                  },
+                  {
+                    headers: {
+                      Authorization: `Bearer ${localStorage.getItem("token")}`,
+                    },
+                  }
+                )
+                .then((res) => {
+                  console.log("res", res);
+                })
+                .catch((err) => {
+                  console.log(err);
+                });
+            }}
+            variant="primary"
+            type="submit"
+          >
+            Submit
+          </Button>
+        </Form>
+      </Container>
+
     </div>
   );
 }
