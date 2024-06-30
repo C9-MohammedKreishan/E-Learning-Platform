7d4aeb7764596005255201b4880fbd032fedc3d5
Mohammmad Kreishan <146862386+MohammedKreishan@users.noreply.github.com>
2024-06-30 10:27:37 +0300

    Modify Landing Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import React, { useState, useContext, createContext } from "react";
 import NavBar from "./components/NavBar/index";
 import Register from "./components/Register/Register";
 import Login from "./components/Login/Login";
-import Signout from "./components/Login/Logout"
+import Signout from "./components/Login/Logout";
 import Dashboard from "./components/Dashboard/Dashboard";
 import AddCourse from "./components/Courses/Courses";
 import Allcourses from "./components/Allcourses/Allcourses";
@@ -12,29 +12,31 @@ import MarketingAndBusiness from "./components/Categories/MarketingAndBusiness";
 import ArtsAndPhotography from "./components/Categories/ArtsAndPhotography";
 import PersonalFinanceAndEconomics from "./components/Categories/PersonalFinanceAndEconomics";
 import Home from "./components/Home/Home";
-import Footer from "./components/Footer/Footer"
-import OneCourse from "./components/Courses/OneCourse"
-import Categories from "./components/Categories/Categories"
+import Footer from "./components/Footer/Footer";
+import OneCourse from "./components/Courses/OneCourse";
+import Categories from "./components/Categories/Categories";
 import RandomCourse from "./components/Courses/RandomCourse";
 import Admin from "./components/Admin/Admin";
-import ContactUs  from "./ContactUs/ContactUs";
+import ContactUs from "./ContactUs/ContactUs";
 import AboutUs from "./components/AboutUs/AboutUs";
-import  FrequentQ from "./components/F&Q/FrequentQ";
+import FrequentQ from "./components/F&Q/FrequentQ";
 export const userContext = createContext();
 
 function App() {
   const [toggle, setToggle] = useState(true);
-  const [role, setRole] = useState("")
+  const [role, setRole] = useState("");
   const [token, setToken] = useState(localStorage.getItem("token") || null);
-  
+
   return (
-    <userContext.Provider value={{ toggle, setToggle, token, setToken,role,setRole }}>
-
+    <userContext.Provider
+      value={{ toggle, setToggle, token, setToken, role, setRole }}
+    >
       <div className="App">
-
         <NavBar />
 
         <Routes>
+        <Route path="/" element={<Home />} />
+
           <Route path="/Home" element={<Home />} />
           <Route path="/users/register" element={<Register />} />
           <Route path="/users/login" element={<Login />} />
@@ -43,16 +45,14 @@ function App() {
           <Route path="/users/aboutUs" element={<AboutUs />} />
           <Route path="/users/frequentq" element={<FrequentQ />} />
 
-
-
-
           <Route path="/users/dashboard" element={<Dashboard />} />
           <Route path="/users/allcourses" element={<Allcourses />} />
           <Route path="/users/onecourse/:courseId" element={<OneCourse />} />
-          <Route path="/users/onecourse/randomcourse" element={<RandomCourse />} />
+          <Route
+            path="/users/onecourse/randomcourse"
+            element={<RandomCourse />}
+          />
           <Route path="/users/admin" element={<Admin />} />
-
-
 
           <Route path="/users/categories" element={<Categories />} />
           <Route
@@ -68,7 +68,7 @@ function App() {
             element={<PersonalFinanceAndEconomics />}
           />
         </Routes>
-        <Footer/>
+        <Footer />
       </div>
     </userContext.Provider>
   );
